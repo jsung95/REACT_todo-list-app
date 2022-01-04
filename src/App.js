7a9f42f5918560b147b1639bbb5a13f955895873
jsung95 <76650814+jsung95@users.noreply.github.com>
2022-01-04 16:19:41 +0900

    Event Handler 추가

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TodoItemList from './components/TodoItemList';
 class App extends Component {
 
   id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
-  
+
   // 상태변수를 가진 state 객체 선언
   state = {
     todo: '',
@@ -18,17 +18,45 @@ class App extends Component {
     ]
   }
 
-
-  render() {
-    return (
-      <div>
-        {/* TodoListTemplate.js 호출 */}
-        <TodoListTemplate form={<Form />}>
-          <TodoItemList />
-        </TodoListTemplate>
-      </div>
-    );
+  //Event Handler 메서드 선언 
+  handleChange = (e) => {
+    this.setState({
+      todo: e.target.value // todo 의 다음 바뀔 값
+    });
   }
-}
+
+  handleCreate = () => {
+    const { todo, todos } = this.state;
+    this.setState({
+      todo: '', // todo 초기화
+      // concat 을 사용하여 배열에 추가
+      todos: todos.concat({
+        id: this.id++,
+        text: todo,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 이면 handleCreate 호출
+    if (e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
+
+
+    render() {
+      return (
+        <div>
+          {/* TodoListTemplate.js 호출 */}
+          <TodoListTemplate form={<Form />}>
+            <TodoItemList />
+          </TodoListTemplate>
+        </div>
+      );
+    }
+  }
 
 export default App;
