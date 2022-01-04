8ae8ac0da6dae87d594026fb8c193a6ecb223f37
jsung95 <76650814+jsung95@users.noreply.github.com>
2022-01-04 16:16:14 +0900

    App.js update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+
+  id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+  
+  // 상태변수를 가진 state 객체 선언
+  state = {
+    todo: '',
+    todos: [
+      { id: 0, text: '리액트 소개', checked: false },
+      { id: 1, text: '리액트 구조', checked: true },
+      { id: 2, text: '리액트 사용', checked: false }
+    ]
+  }
+
+
   render() {
     return (
       <div>
