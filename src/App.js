6331eb578e14bd236ac0bae86788ecd2a9661247
jsung95 <76650814+jsung95@users.noreply.github.com>
2022-01-04 16:04:13 +0900

    todo list
    틀생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import './components/Form.css';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* TodoListTemplate.js 호출 */}
+        <TodoListTemplate form={<Form />}>
+          <TodoItemList />
+        </TodoListTemplate>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoItemList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+    
+    render() {
+        const {todos, myToggle, myRemove} = this.props;
+        return (
+            <div>
+                <TodoItem text="todo1" checked={false}/>
+                <TodoItem text="todo2" checked={true}/>
+                <TodoItem text="todo3" checked={false}/>
+            </div>
+        );
+    }
+}
+
+
+export default TodoItemList;
